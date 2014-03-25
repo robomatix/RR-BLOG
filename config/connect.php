465038ed43be4f27335ee138d6165f0c3e90498e
Robomatix Rebirth <robomatix@robomatix.net>
2014-03-25 08:17:56 +0100

    Delete connect.php

--- config/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
-define('_MYSQL_SET_SQL_MODE',true);
-$GLOBALS['spip_connect_version'] = 0.7;
-spip_connect_db('mysql51-76.bdb','','robomatiblog','pouet69','robomatiblog','mysql', 'spip','');
-?>
