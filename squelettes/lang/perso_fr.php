91a4e497659f0a04dc2f31510d5cd4045a09a07a
robomatix <robomatix@robomatix>
2014-05-04 12:03:56 +0200

    Mise en place système articles connexes

---
+++ squelettes/lang/perso_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/comments/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'article_connexe' => 'article connexe',
+    	'articles_connexes' => 'articles connexes',
+);
+
+
